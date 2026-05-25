30448a1dc70311a3c1b7aa580c122bb189061569
thekilledone1 <vavavava303030@gmail.com>
2026-05-25 15:42:45 -0400

    Delete RobloxClientAssistant.bat

--- RobloxClientAssistant.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Unblocks both files to prevent Windows security warnings on future runs.
-powershell.exe -NoProfile -Command "Unblock-File -Path '%~dp0RobloxClientAssistant.bat'; Unblock-File -Path '%~dp0RobloxClientAssistant.ps1'"
-:: Launches RobloxClientAssistant.ps1 with no console window.
-:: Keep this .bat in the same folder as the .ps1 file.
-powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0RobloxClientAssistant.ps1"
